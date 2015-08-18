77085a5d784003df079d374e75f1943b8a86da1a
nickells <ellsworth.nick@gmail.com>
2015-08-18 12:49:37 -0400

    Dragon

--- browser/js/common/directives/ui-components/ui-thumbnail/ui-thumbnail.js
+++ browser/js/common/directives/ui-components/ui-thumbnail/ui-thumbnail.js
@@ -1,8 +1,8 @@
-app.directive("uiThumbnail", function($rootScope, $compile) {
+app.directive("uiThumbnail", function($rootScope) {
 	return {
 		restrict: "E",
 		scope: {
-			component: "="
+			component: "=",
 		},
 		templateUrl: 'js/common/directives/ui-components/ui-thumbnail/ui-thumbnail.html',
 		link: function(scope, elem, attr) {
@@ -10,15 +10,59 @@ app.directive("uiThumbnail", function($rootScope, $compile) {
 			console.log(attr);
 			console.log("THUMBNAIL", scope.component.thumbnail);
 			elem.css('background-image', "url(" + scope.component.thumbnail + ")");
+			elem.addClass('thumb')
 			//can hook into elements parent and place html of template into it.
-			elem.click(function() {
-				var toAppend = $compile(scope.component.html)(scope);
-				$("#app").append(toAppend);
-				// $rootScope.$apply();
-				// $rootScope.$digest();
-				console.log("ELEMENT", elem, "ELEMENT PARENT", elem.parent());
-				console.log("WHAT THIS WORKED")
-			})
+			// elem.click(function() {
+			// 	var toAppend = $compile(scope.component.html)(scope);
+			// 	$("#app").append(toAppend);
+			// 	console.log("ELEMENT", elem, "ELEMENT PARENT", elem.parent());
+			// 	console.log("WHAT THIS WORKED")
+			// })
+			interact('.thumb')
+				.draggable({
+			    // enable inertial throwing
+					inertia: true,
+				    // keep the element within the area of it's parent
+				    // restrict: {
+				    //   restriction: "parent",
+				    //   endOnly: true,
+				    //   elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+				    // },
+
+				    // call this function on every dragmove event
+					onstart: function(event){
+				    	console.log("clicked")
+				    	console.log(event.target)
+				    	$(event.target).clone().appendTo('#elements');
+					},
+					onmove: dragMoveListener,
+				    // call this function on every dragend event
+					onend: function (event) {
+				    	$(event.target).remove();
+					}
+				      // var textEl = event.target.querySelector('p');
+
+			      // textEl && (textEl.textContent =
+			      //   'moved a distance of '
+			      //   + (Math.sqrt(event.dx * event.dx +
+			      //                event.dy * event.dy)|0) + 'px');
+			});
+
+			function dragMoveListener (event) {
+				var target = event.target,
+			        // keep the dragged position in the data-x/data-y attributes
+			    	x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+			    	y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+			    // translate the element
+				target.style.webkitTransform =
+				target.style.transform =
+					'translate(' + x + 'px, ' + y + 'px)';
+
+			    // update the posiion attributes
+				target.setAttribute('data-x', x);
+				target.setAttribute('data-y', y);
+			}
 		}
 	}
 })
--- browser/js/edit-page/edit-page.js
+++ browser/js/edit-page/edit-page.js
@@ -9,7 +9,7 @@ app.config(function($stateProvider) {
 
 });
 
-app.controller('EditPageController', function($scope, UILibraryFactory) {
+app.controller('EditPageController', function($scope, $compile, UILibraryFactory) {
 	// console.log(UILibraryFactory.uiNavbar);
 	$scope.uiLibrary = UILibraryFactory;
 	// $scope.uiNavbar = {
@@ -18,4 +18,22 @@ app.controller('EditPageController', function($scope, UILibraryFactory) {
 	$scope.parseTree = {};
 	$scope.addToParseTree = function() {};
 	$scope.createReactClass = function() {};
+
+	interact('#app')
+		.dropzone({
+			ondrop: function(event){
+				// console.log($scope.UILibrary[event.component])
+				var thisComponentName = $(event.relatedTarget).attr('component').split(".")[1];
+				console.log (thisComponentName);
+				// var componentObject = eval("$scope." + thisComponentName);
+				// console.log(thisComponentName);
+				// console.log("mouseup","in",event)
+				var toAppend = $compile($scope.uiLibrary[thisComponentName].html)($scope);
+				$(event.target).append(toAppend);
+				// console.log("ELEMENT", elem, "ELEMENT PARENT", elem.parent());
+				console.log("WHAT THIS WORKED")
+					
+			}
+		})
+
 });
