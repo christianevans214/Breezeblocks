f2d38d25c6ec752724ce802c1d4a7f33d09a6ab2
Christian Evans <christian.evans214@gmail.com>
2015-08-17 20:25:42 -0400

    added navbar

--- browser/js/common/factories/UILibraryFactory.js
+++ browser/js/common/factories/UILibraryFactory.js
@@ -1,7 +1,7 @@
 app.factory("UILibraryFactory", function() {
 	return {
 		uiNavbar: {
-			html: "<h1> Hey </h1>",
+			html: "<h1> NAV BAR NAV BAR NAV BAR NAV BAR NAV BAR NAV BAR NAV BAR NAV BAR NAV </h1>",
 			jsx: "hey",
 			thumbnail: "/images/navbar.png"
 		}
