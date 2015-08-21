5db38030390a1f7d5e9e7cf2e5d42866ba9f773d
Patrick Trasborg <PT@LMED0347.local>
2015-08-21 15:43:55 -0400

    cleaned up files

--- server/app/reactUtils/generator.js
+++ server/app/reactUtils/generator.js
@@ -1,18 +1,150 @@
-var handlebars = require('handlebars');
-var fs = require('fs');
+var Handlebars = require('Handlebars');
+var ChangeCase = require('change-case');
 
-var template = handlebars.compile(fs.readFileSync('/Users/PT/Fullstack/DrandAndDrop/server/app/reactUtils/template.js', 'utf8'));
+var data = [
+	{
+		className: ['drop-area','view-1'],
+		children: [
+			{type: 'Navbar',
+			 className: ['ui-navbar', 'view-1-navbar-1'],
+			 props: [
+			 	{ "name": "title", "value": "my cool app", type: "string" }
+			 ]
+			},
+			{type: 'Navbar',
+			 className: ['ui-navbar', 'view-1-navbar-2'],
+			 props: [
+			 	{ "name": "title", "value": "my okay app", type: "string" }
+			 ]
+			},
+			{type: 'Navbar',
+			 className: ['ui-navbar', 'view-1-navbar-3'],
+			 props: [
+			 	{ "name": "title", "value": "my bad app", type: "string" }
+			 ]
+			},
+			{type: 'Navbar',
+			 className: ['ui-navbar', 'view-1-navbar-3'],
+			 props: [
+			 	{ "name": "title", "value": "the title", type: "string" }
+			 ]
+			}
 
-var output = template({
-	renderElements: "<View style={[styles.container, styles.test]}><BasicScrollView style={[styles.somerandomclass, styles.somerandomclass2]} horizontally='true'/><BasicScrollView style={[styles.someotherclass, styles.someotherclass2]} horizontally='false'/></View>",
-	styleObject: "container: {flex: 1,justifyContent: 'center',alignItems: 'center',backgroundColor: '#F5FCFF',},"
+		]
+	},
+	{
+		className: ['drop-area','view-2'],
+		children: [
+			{type: 'Image',
+			 className: ["ui-image",'view-2-image-1'],
+				 props: [
+			 	{ "name": "source", "value": "http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpg", type: "string" }
+			 ]
+			},
+			{type: 'Image',
+			 className: ['ui-image', 'view-2-image-2'],
+			 props: [
+			 	{ "name": "source", "value": "https://imgs.xkcd.com/comics/perl_problems.png", type: "string" }
+			 ]	
+			}			 
+		]
+
+	}
+]
+
+var styleData = {
+    "view1": {
+        "flex": '1',
+        "justify-content": 'center',
+        "align-items": 'center',
+        "background-color": '#F5FCFF',
+    },
+    "img1": {
+        "width": '200px',
+        "height": '200px',
+    },
+    "view2": {
+        "flex": '1',
+        "justify-content": 'center',
+        "align-items": 'center',
+        "background-color": '#F5FCFF',
+    },
+    "img2": {
+        "width": '200px',
+        "height": '200px',
+    }
+};
+
+var templateFile = require('fs').readFileSync('./template.hbs').toString();
+
+// Handlebars.registerPartial('View', require('fs').readFileSync('./testPartial.hbs'));
+
+Handlebars.registerHelper('getProp', function (propObj) {
+	if(propObj.name === "source") return "{{uri: '" + propObj.value + "'}}";
+	else if(propObj.type === "string") return "'" + propObj.value + "'";
+	else return propObj.value; 
+});
+
+Handlebars.registerHelper('camelCase', function (string) {
+	return ChangeCase.camelCase(string);
+});
+
+Handlebars.registerHelper('removePx', function(string){
+	string = string.replace(/px$/,"");	
+	if(string.match(/[^0-9]/) !== null) return "'" + string + "'";
+	else return string;
 })
 
-console.log("output");
-fs.writeFile('/Users/PT/Fullstack/DrandAndDrop/server/app/reactUtils/renderedTemplate.js', output, function(err){
-	if(err) throw err;
-	console.log('filed saved')
-})
+
+var createTemplate = Handlebars.compile(templateFile);
+
+var renderedTemplate = createTemplate({ tree: data, styleTree: styleData });
+
+require('fs').writeFile('./reactNative/index.ios.js', renderedTemplate, console.error.bind(console));
 
 
+//recursive version
+/*	var reactDOM = "";
+	for(var i = 0; i < json.length; i++){
+	    reactDOM += domMaker(json[i]);
+	}
+	return reactDOM;
+
+	function domMaker(component){
+	    var domEle="";
+	    var key = Object.keys(component)[0];
+	    var prop = "";
+	    var style = "";
+	    var props = component[key].props;
+	    var classNames = component[key].className;
+	    var children = component[key].children;
+	    
+	    for(var propKey in props){
+	        if(typeof props[propKey] !== Number){
+	            prop += " " + propKey + "='" + props[propKey] + "'";  
+	        }else{
+	            prop += " " + propKey + "=" + props[propKey];  
+	        }
+	    }
+	    
+	    var className = " style={[";
+	    for(var j = 0; j<classNames.length; j++){
+	        className += "styles." + classNames[j];
+	        if(j<classNames.length-1) className += ", ";
+	    }
+	    className += "]}";
+	    
+	    domEle += "<" + key + className + prop;
+	    for(var j = 0; j<children.length; j++){
+	        if(j===0) domEle += ">";
+	        domEle += domMaker(children[j]);
+	    }
+	    
+	    if(children.length === 0){
+	        domEle += "/>";
+	    }else{
+	        domEle += "</" + key + ">";
+	    }
+	    return domEle;
+	}*/
 
--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,38 +3,6 @@ var router = require('express').Router();
 var path = require('path');
 module.exports = router;
 
-var exampleData = [
-                {
-                    className: ['drop-area','view-1'],
-                    children: [
-                        {type: 'Navbar',
-                        className: ['ui-navbar', 'view-1-navbar-1'],
-                        content: ['My Cool App']
-                        },
-                        {type: 'Navbar',
-                        className: ['ui-navbar', 'view-1-navbar-2'],
-                        content: ['My Okay App']
-                        },
-                        {type: 'Navbar',
-                        className: ['ui-navbar', 'view-1-navbar-3'],
-                        content: ['My Bad App :(']
-                        }
-
-                    ]
-                },
-                {
-                    className: ['drop-area','view-2'],
-                    children: [
-                        {type: 'Image',
-                        className: ["ui-image",'view-2-image-1'],
-                        content: ['http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpg']},
-                        {type: 'Image',
-                        className: ['ui-image', 'view-2-image-2'],
-                        content: ['https://imgs.xkcd.com/comics/perl_problems.png']}
-                    ]
-
-                }
-            ];
 
 var pathToTemplate = path.join(__dirname, './../reactUtils/template.html');
 var pathToGenerator = path.join(__dirname, './../reactUtils/generator.js');
