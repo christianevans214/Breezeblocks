472395e511a93d69d6951eb8ee8bdc1a6dc9d055
Carolyn Zelenetz <carolyn.zelenetz@gmail.com>
2015-08-21 16:52:57 -0400

    added routes for users and apps

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,23 +3,9 @@ var router = require('express').Router();
 var path = require('path');
 module.exports = router;
 
-
-var pathToTemplate = path.join(__dirname, './../reactUtils/template.html');
-var pathToGenerator = path.join(__dirname, './../reactUtils/generator.js');
-
 router.use('/members', require('./members'));
-
-router.get('/view', function(req, res){
-	res.send(exampleData);
-});
-
-router.get('/template', function(req, res){
-	res.sendFile(pathToTemplate);
-});
-
-router.get('/generator', function(req, res){
-	res.sendFile(pathToGenerator);
-})
+router.use('/build', require('./build'));
+router.use('/users', require('./users'));
 
 // Make sure this is after all of
 // the registered routes!
