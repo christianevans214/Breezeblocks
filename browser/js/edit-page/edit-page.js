0ca8a3d25e80d9309db452012c96210469f67a90
Christian Evans <christian.evans214@gmail.com>
2015-08-22 11:08:11 -0400

    parent dropbox can delete

--- browser/js/edit-page/edit-page.js
+++ browser/js/edit-page/edit-page.js
@@ -20,6 +20,12 @@ app.controller('EditPageController', function($scope, $compile, UILibraryFactory
 	$scope.convertObjToInlineStyle = CssTreeFactory.objToInlineStyle;
 	$scope.cssTree = CssTreeFactory.cssTree;
 	$scope.parseTree = ParseTreeFactory.parseTree.tree;
+	$scope.uiLibrary = UILibraryFactory;
+	//properties to edit styling:
+	$scope.activeCSSEdit = {};
+	//properties to edit HTML
+	$scope.activeHTMLEdit = {};
+	$scope.currentlySelected = null;
 
 	$scope.pathName = function(elemPath) {
 		return "js/common/components/" + elemPath + ".html"
@@ -41,12 +47,7 @@ app.controller('EditPageController', function($scope, $compile, UILibraryFactory
 	}
 
 
-	$scope.uiLibrary = UILibraryFactory;
-	//properties to edit styling:
-	$scope.activeCSSEdit = {};
-	//properties to edit HTML
-	$scope.activeHTMLEdit = {};
-	$scope.currentlySelected = null;
+
 	$scope.lessFlex = StyleFactory.lessFlex($scope);
 	$scope.moreFlex = StyleFactory.moreFlex($scope);
 	$scope.deleteElem = function() {
@@ -63,28 +64,6 @@ app.controller('EditPageController', function($scope, $compile, UILibraryFactory
 		$scope.$digest();
 	}
 
-	//delete button code, not quite working yet:
-	// $scope.showDeleteButton = function(className){
-	// var $elem = $('.'+className)
-	// $elem.prepend('<span style="display: inline-block; position: absolute; float: left; align-self: flex-start;" class="x-button">x</span>')
-	// $('.x-button').on('click',function(){
-	// 	ParseTreeFactory.removeRow($scope, className)
-	// 	$(this).remove();
-	// })
-	// }
-
-	// $scope.hideDeleteButton = function(className){
-	// 	$('.x-button').remove();
-	// }
-
-	//event emitter to catch whenever someone selects a template in the iPhone
-	// $scope.$on('changeSelect', function(event, data) {
-	// 	if($scope.currentlySelected) $scope.currentlySelected.removeClass('shadow')
-	// 	$scope.currentlySelected = $(data)
-	// 	$scope.currentlySelected.addClass('shadow')
-	// 	$scope.editProps = {flexGrowSize: 1};
-	// 	$scope.$apply()
-	// });
 	EmitterizerFactory.makeEmitterListeners($scope);
 
 	Interactory.Interact($scope);
