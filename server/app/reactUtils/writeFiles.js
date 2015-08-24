ed537ae80dc70b903e5b0bde2f20df68856efca1
Carolyn Zelenetz <carolyn.zelenetz@gmail.com>
2015-08-24 14:59:26 -0400

    cleaned up files

--- server/app/reactUtils/writeFiles.js
+++ server/app/reactUtils/writeFiles.js
@@ -23,23 +23,3 @@ module.exports = function(userId, buildId, repo, projectName){
 		console.log("promise error", err); 
 	});
 }
-
-/*		var i = 0;
-		var writingFiles = setInterval(function(){
-			var fileName = keys[i]
-			repo.write('master', fileName, fileObject[fileName], 'Exported BreezeBlocks Project', function(err) {
-				console.log("writing to file", i+1);
-				if(err) console.error(err);
-			});
-			i++;
-			if(i >= keys.length) clearInterval(writingFiles);
-		}, 1000);*/
-/*function repoWrite(fileNames, fileObject, index){
-	repo.write('master', fileNames[index], fileObject[fileNames[index]], 'Exported BreezeBlocks Project', function(err) {
-		console.log("writing to file", index);
-		if(err) console.error(err);
-		else if(index < fileNames.length) {
-			repoWrite(fileNames, fileObject, index++);
-		}
-	});
-}*/
