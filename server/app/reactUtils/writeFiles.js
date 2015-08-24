1f0bb61794538ff5462b3851fff7d99821cb7f26
Patrick Trasborg <patrick.trasborg@gmail.com>
2015-08-24 15:27:05 -0400

    added error handling to run error files

--- server/app/reactUtils/writeFiles.js
+++ server/app/reactUtils/writeFiles.js
@@ -3,6 +3,7 @@ var path = require('path');
 
 module.exports = function(userId, buildId, repo, projectName){
 	console.log("Begin Writing");
+	var erroredFiles = {};
 	fileContent(userId, buildId, projectName)
 	.then(function(fileObject){
 		var keys = Object.keys(fileObject);
@@ -11,18 +12,40 @@ module.exports = function(userId, buildId, repo, projectName){
 			console.log("fileNames", index, fileNames[index])
 			repo.write('master', fileNames[index], fileObject[fileNames[index]], 'Exported BreezeBlocks Project', function(err) {
 				console.log("writing to file", index);
-				if(err) console.error(err);
+				if(err){
+					erroredFiles[fileNames[index]] = fileObject[fileNames[index]];
+					console.error(err);
+				}
+
 				index++;
+
 				if(index < fileNames.length) {
 					repoWrite(fileNames, fileObject, index);
 				}
 			});
+
+
 		})(keys, fileObject, 0);
+
+		return erroredFiles;
+	})
+	.then(function(erroredFiles){
+		var newKeys = Object.keys(erroredFiles);
+		if(newKeys.length > 0){
+			var keyCount = 0;
+			var errorInterval = setInterval(function(){
+				repo.write('master', newKeys[keyCount], erroredFiles[newKeys[keyCount]], 'Exported Breezeblocks Project', function(err){
+					if(err) console.log("There was still an error writing to github");
+				});
+				keyCount++;
+				if(keyCount >= newKeys.length) clearInterval(errorInterval);
+			}, 1000);
+		}
 	})
 	.catch(function(err){ 
 		console.log("promise error", err); 
 	});
-}
+};
 
 /*		var i = 0;
 		var writingFiles = setInterval(function(){
