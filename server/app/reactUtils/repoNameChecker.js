e8b92321d78f6654d516a9620cfe6b44eedce834
Carolyn Zelenetz <carolyn.zelenetz@gmail.com>
2015-08-27 19:51:40 -0400

    adding repo name check

---
+++ server/app/reactUtils/repoNameChecker.js
@@ -0,0 +1,13 @@
+var shortid = require('shortid');
+module.exports = function(currentUser, github, repoName){
+	//if user already has github repo with repo name, change repo name
+	return new Promise(function(resolve, reject){
+		if(!currentUser){
+			reject(currentUser);
+		}
+		if(github.getRepo(currentUser.github.username, repoName)){
+			repoName = repoName + '_' + shortid.generate;
+			resolve(repoName);
+		}
+	})
+}
