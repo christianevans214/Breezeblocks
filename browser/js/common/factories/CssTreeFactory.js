89ecce21e807ce1419611c5fabee7191cec0303c
nickells <ellsworth.nick@gmail.com>
2015-08-28 10:58:36 -0400

    added placeholder styles

--- browser/js/common/factories/CssTreeFactory.js
+++ browser/js/common/factories/CssTreeFactory.js
@@ -1,6 +1,6 @@
-app.factory('CssTreeFactory', function($http) {
+app.factory('CssTreeFactory', function ($http) {
 	return {
-		objToInlineStyle: function(className, $scope) {
+		objToInlineStyle: function (className, $scope) {
 			var str = ""
 			for (var selector in this.project.css[className]) {
 				str += selector + ': ' + this.project.css[className][selector];
@@ -9,22 +9,36 @@ app.factory('CssTreeFactory', function($http) {
 			}
 			return str
 		},
-		addViewClass: function(className, $scope) {
+		addViewClass: function (className, $scope) {
 			$scope.project.css[className] = {};
 		},
-		addChildClass: function(className, $scope) {
+		addChildClass: function (className, $scope) {
 			$scope.project.css[className] = {
-				"flex-grow": 1
+				"flex-grow": 1,
+				"padding-left": 0,
+				"padding-right": 0,
+				"padding-top": 0,
+				"padding-bottom": 0,
+				"margin-left": 0,
+				"margin-right": 0,
+				"margin-top": 0,
+				"margin-bottom": 0
 			};
+
+			if (className.match(/(text)|(listview)|(navbar)/)) {
+				console.log("hey", className)
+				console.log($scope.project.css[className])
+				$scope.project.css[className]['font-size'] = 17;
+			}
 		},
-		removeClass: function(className, $scope) {
+		removeClass: function (className, $scope) {
 			console.log("className to delete", className);
 			console.log("BEFORE DELETE", $scope.project.css);
 			console.log("CLASS NAME PROPERTIES", $scope.project.css[className])
 			if ($scope.project.css[className]) delete $scope.project.css[className];
 			console.log("AFTER DELETE", $scope.project.css);
 		},
-		removeViewClass: function(rowClassName, $scope) {
+		removeViewClass: function (rowClassName, $scope) {
 			var classDeleteArr = [rowClassName];
 			var classNameRegex = new RegExp(rowClassName)
 				//need to find Children based on parent className
